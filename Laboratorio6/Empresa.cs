60c997a725ccf7521fe130913964dd5a86695918
Juan Jacobo Giraldo Rodriguez <juanjacobogiraldorodriguez@Juans-MacBook-Air-2.local>
2020-05-18 22:34:48 -0400

    Parte 1 Sketch // Falta solo el manejo de excepciones

---
+++ Laboratorio6/Empresa.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Laboratorio6
+{
+    [Serializable]
+    public class Empresa
+    {
+        protected string Nombre_Empresa;
+        protected string Rut_Empresa;
+
+        public Empresa(string N, string R)
+        {
+            Nombre_Empresa = N;
+            Rut_Empresa = R;
+        }
+
+         public string IETS()
+         {
+            return "Nombre de la Empresa: " + Nombre_Empresa + "\n" + "Rut de la Empresa: " + Rut_Empresa;
+         }
+        
+    }
+}
--- Laboratorio6/Program.cs
+++ Laboratorio6/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace Laboratorio6
 {
@@ -6,7 +10,81 @@ namespace Laboratorio6
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Porfin Abrio");
+            string Menu = "Bienvenido a su administrador de personal generico.";
+            Console.WriteLine(Menu);
+
+            List<Empresa> EmpresaX = new List<Empresa>();
+
+            string m1 = " Desea cargar el archivo de empresas? (Si/No)";
+            string m2 = " Deseas crear una empresa y agregarla al archivo? (Si/No)";
+            while (true)
+            {
+                Console.WriteLine(m1);
+                string choice = Console.ReadLine();
+
+                if (choice == "Si")
+                {
+                    EmpresaX = Load();
+                    if (EmpresaX.Count == 0)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("El archivo esta vacio, no hay niguna empresa creada");
+                        Console.WriteLine(m2);
+                        string ichoice = Console.ReadLine();
+                        if (ichoice == "Si")
+                        {
+                            añadirEmpresa(EmpresaX);
+                            Save(EmpresaX);
+                        }
+                        else if (ichoice == "No")
+                        {
+                            break;
+                        }
+                    }
+                    else
+                    {
+                       foreach (Empresa empresa in EmpresaX)
+                            Console.WriteLine(empresa.IETS());
+                    }
+                }
+                else if (choice == "No")
+                {
+                    añadirEmpresa(EmpresaX);
+                    Save(EmpresaX);
+                    break;
+                }
+
+            }
+
         }
+        static public void añadirEmpresa(List<Empresa> empresax)
+        {
+            Console.Write("Nombre de la Empresa: ");
+            string nom = Console.ReadLine();
+            Console.Write("Rut de la Empresa: ");
+            string rut = Console.ReadLine();     
+            empresax.Add(new Empresa(nom, rut));
+        }
+
+        static private void Save(List<Empresa> empresas)
+        {
+            IFormatter formatter = new BinaryFormatter();
+            Stream stream = new FileStream("empresa.bin", FileMode.Create, FileAccess.Write, FileShare.None);
+            formatter.Serialize(stream, empresas);
+            stream.Close();
+        }
+
+        static private List<Empresa> Load()
+        {
+            IFormatter formatter = new BinaryFormatter();
+            Stream stream = new FileStream("empresa.bin", FileMode.Open, FileAccess.Read, FileShare.Read);
+            List<Empresa> empresa = (List<Empresa>)formatter.Deserialize(stream);
+            stream.Close();
+            return empresa;
+        }
+
+
+
+
     }
 }
